f8df9c7a30f16ebc741e7c5e164fc03df1b3ab36
Lukas Menzel <lukas@menzel.earth>
2020-10-03 13:27:49 +0200

    prettier

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,4 +2,5 @@ declare module "*.vue" {
   import { defineComponent } from "vue";
   const component: ReturnType<typeof defineComponent>;
   export default component;
+  // export { component as default };
 }
