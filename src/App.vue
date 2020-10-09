dd1c4ac4f11bcc2225f3895bb0a1281c743a3e41
Lukas Menzel <lukas@menzel.earth>
2020-10-09 10:35:44 +0200

    move styles

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,36 @@ export { default as TodoList } from "@/components/TodoList.vue";
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.button {
+  margin-left: 1rem;
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.125rem;
+  border: 1px solid black;
+  background: transparent;
+  box-sizing: border-box;
+  cursor: pointer;
+}
+
+.button-group .button {
+  margin-left: 0;
+  border-radius: 0;
+  border-left: none;
+}
+
+.button-group .button:first-child {
+  border-left: 1px solid black;
+  border-top-left-radius: 0.125rem;
+  border-bottom-left-radius: 0.125rem;
+}
+
+.button-group .button:last-child {
+  border-top-right-radius: 0.125rem;
+  border-bottom-right-radius: 0.125rem;
+}
+
+.button-group {
+  display: inline-block;
+  margin-left: 1rem;
+}
 </style>
